60d12e6abdd25ce0fbcb394c8c4f8a758d3ccbb9
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-06-15 15:19:58 -0700

    added some stuff

--- src/App.js
+++ src/App.js
@@ -7,34 +7,30 @@ import Goblin from './Goblin';
 function App() {
   const [allGoblins, setAllGoblins] = useState([]);
   const [visibleGoblins, setVisibleGoblins] = useState(allGoblins);
-  const [goblinFormName, setGoblinFormName] = useState();
-  const [goblinFormHP, setGoblinFormHP] = useState();
-  const [goblinFormColor, setGoblinFormColor] = useState('');
-  /* 
-    track: 
-      allGoblins, an array of all goblins
-      visibleGoblins, a second array of goblins: this one is the filtered version of the above allGoblins array
-      goblinFormName, which is how we track the user input for the current name of the goblin in the form
-      goblinFormHP, which is how we track the user input for the current HP of the goblin in the form
-      goblinFormColor, which is how we track the user input for the current color of the goblin in the form
-*/
-  
+  const [goblinFormName, setGoblinFormName] = useState('');
+  const [goblinFormHP, setGoblinFormHP] = useState('');
+  const [goblinFormColor, setGoblinFormColor] = useState();
+ 
+  console.log(allGoblins);
+
   function submitGoblin(e) {
     e.preventDefault();
-    
-    // on submit, make a new goblin object with a name that comes from the form state, an hp that comes from the form state, and a color that comes from the form state
-
-    // update the allGoblins array. Add the new goblin to the allGoblins array immutably.
-    
-    // clear out the goblin form state items by setting them to empty strings. This will cause the form to reset in the UI.
+    const goblin = {
+      name: goblinFormName,
+      HP: goblinFormHP,
+      color: goblinFormColor
+    };
+    const newGoblin = [...allGoblins, goblin];
+    setAllGoblins(newGoblin);
   }
 
   function handleDeleteGoblin(name) {
     // find the index of the goblin in allGoblins with this name
-
+    const index = allGoblins.findIndex(goblin => goblin.name === name);
     // use splice to delete the goblin object at this index
-
+    allGoblins.splice(index, 1);
     // update the allGoblins array immutably to this new, smaller array
+    setAllGoblins(allGoblins);
   }
 
   function handleFilterGoblins(search) {
@@ -49,11 +45,11 @@ function App() {
     <div className="App">
       <div className='current-goblin quarter'>
         <Goblin goblin={{
-          /* 
-            use the goblin form state to make a goblin object and to display it. 
-            This will let the user see the current form state 
-          */
-        }}/>
+          color: goblinFormColor,
+          name: goblinFormName,
+          HP: goblinFormHP
+        }}
+        handleDeleteGoblin={handleDeleteGoblin} />
       </div>
       <div className='goblin-filter quarter'>
         Filter Goblins
@@ -61,20 +57,18 @@ function App() {
         <input onChange={(e) => handleFilterGoblins(e.target.value)} />
       </div>
       <GoblinForm 
-        /*
-        This component takes in a ton of props! 
-        Here is the list of props to pass:
-          submitGoblin,
-          goblinFormName, 
-          setGoblinFormName,
-          goblinFormColor, 
-          setGoblinFormColor,
-          goblinFormHP, 
-          setGoblinFormHP,
-        */
+       
+        submitGoblin={submitGoblin}
+        goblinFormName={goblinFormName} 
+        setGoblinFormName={setGoblinFormName}
+        goblinFormColor={goblinFormColor} 
+        setGoblinFormColor={setGoblinFormColor}
+        goblinFormHP={goblinFormHP} 
+        setGoblinFormHP={setGoblinFormHP}
+       
       />
       <GoblinList 
-        goblins={[]} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array 
+        goblins={allGoblins} // this takes in an array of goblins. If the filteredGoblins has a length, use that array. Otherwise, use the allGoblins array 
         handleDeleteGoblin={handleDeleteGoblin} // note that the goblin list has access to the ability to delete
       />
     </div>
